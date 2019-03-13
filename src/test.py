7ea7d7901a1dcf2c5f81e61ea6706fc2d0112785
fdreyer <frederic.dreyer@gmail.com>
2019-03-13 18:51:01 +0000

    added normalisation step

--- src/test.py
+++ src/test.py
@@ -3,6 +3,32 @@ from JetTree import *
 import matplotlib.pyplot as plt
 from gan import GAN
 
+def zca_whiten(X):
+    """
+    Applies ZCA whitening to the data (X)
+    http://xcorr.net/2011/05/27/whiten-a-matrix-matlab-code/
+
+    X: numpy 2d array
+        input data, rows are data points, columns are features
+
+    Returns: ZCA whitened 2d array
+    """
+    assert(X.ndim == 2)
+    EPS = 10e-5
+
+    #   covariance matrix
+    cov = np.dot(X.T, X)
+    #   d = (lambda1, lambda2, ..., lambdaN)
+    d, E = np.linalg.eigh(cov)
+    #   D = diag(d) ^ (-1/2)
+    D = np.diag(1. / np.sqrt(d + EPS))
+    #   W_zca = E * D * E.T
+    W = np.dot(np.dot(E, D), E.T)
+
+    X_white = np.dot(X, W)
+
+    return X_white
+
 nev = 20000
 npxlx = 10
 npxly = 10
@@ -17,6 +43,9 @@ for i, jet in enumerate(events):
     lundimages[i]=li_gen(tree).reshape(npxlx, npxly, 1)
 
 print(lundimages.shape)
+
+# normalisation of images
+lundimages = (lundimages - np.average(lundimages, axis=0))
 
 # plt.imshow(np.average(lundimages, axis=0).transpose(),
 #            origin='lower', aspect='auto')
