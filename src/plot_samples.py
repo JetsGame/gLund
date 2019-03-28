3a704ff650dd3b03cd8c3ffcf4d731a9e6b40fc8
fdreyer <frederic.dreyer@gmail.com>
2019-03-28 13:36:59 +0000

    updating plotting routine

--- src/plot_samples.py
+++ src/plot_samples.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-def plot_sample(filename):
+from matplotlib.backends.backend_pdf import PdfPages
+def plot_mnist(filename):
     r, c = 5, 5
     imgs = np.load(filename)
     imgs[np.random.choice(imgs.shape[0], r*c, replace=False), :]
@@ -16,9 +17,36 @@ def plot_sample(filename):
     fig.savefig(figname)
     plt.close()
 
+def plot_lund(filename):
+    r, c = 5, 5
+    imgs = np.load(filename)
+    imgs[np.random.choice(imgs.shape[0], r*c, replace=False), :]
+    figname=filename.split(os.extsep)[0]+'.pdf'
+    with PdfPages(figname) as pdf:
+        fig, axs = plt.subplots(r, c)
+        cnt = 0
+        for i in range(r):
+            for j in range(c):
+                axs[i,j].imshow(imgs[cnt, :,:], cmap='gray')
+                axs[i,j].axis('off')
+                cnt += 1
+        pdf.savefig()
+        plt.close()
+        fig=plt.figure()
+        imgs = np.load(filename)
+        print(imgs.shape)
+        plt.imshow(np.average(imgs,axis=0))
+        plt.close()
+        pdf.savefig(fig)
 
-plot_sample('test/mnist_dcgan.npy')
-plot_sample('test/mnist_gan.npy')
-plot_sample('test/mnist_vae.npy')
-plot_sample('test/mnist_wgan.npy')
-plot_sample('test/mnist_wgangp.npy')
+plot_mnist('test/mnist_dcgan.npy')
+plot_mnist('test/mnist_gan.npy')
+plot_mnist('test/mnist_vae.npy')
+plot_mnist('test/mnist_wgan.npy')
+plot_mnist('test/mnist_wgangp.npy')
+
+plot_lund('test/lund_dcgan.npy')
+plot_lund('test/lund_gan.npy')
+plot_lund('test/lund_vae.npy')
+plot_lund('test/lund_wgan.npy')
+plot_lund('test/lund_wgangp.npy')
