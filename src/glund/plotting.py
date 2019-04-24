53e098edf9c290a70b80098f00d35964a28a9c0c
fdreyer <frederic.dreyer@gmail.com>
2019-04-24 13:59:24 +0100

    cleaned up plotting routine

--- src/glund/plotting.py
+++ src/glund/plotting.py
@@ -7,6 +7,9 @@ import numpy as np
 from matplotlib.backends.backend_pdf import PdfPages
 
     
+xval = [0.0, 7.0]
+yval = [-3.0, 7.0]
+
 #----------------------------------------------------------------------
 def plot_lund(filename, figname):
     """Plot a few samples of lund images as well as the average density."""
@@ -18,7 +21,8 @@ def plot_lund(filename, figname):
         cnt = 0
         for i in range(r):
             for j in range(c):
-                axs[i,j].imshow(sample[cnt, :,:].transpose(), origin='lower', cmap='gray',vmin=0.0,vmax=2.0)
+                axs[i,j].imshow(sample[cnt, :,:].transpose(), origin='lower',
+                                cmap='gray', vmin=0.0, vmax=2.0)
                 axs[i,j].axis('off')
                 cnt += 1
         pdf.savefig()
@@ -65,23 +69,33 @@ def plot_lund_with_ref(filename, reference, figname):
                 cnt += 1
         pdf.savefig()
         plt.close()
-        fig=plt.figure()
+        fig=plt.figure(figsize=(6,6))
+        cbartics   = [0, 0.05, 0.1, 0.15, 0.2, 0.25]
         plt.title('generated')
-        plt.imshow(np.average(imgs,axis=0).transpose(), origin='lower',vmin=0.0,vmax=0.7)
-        plt.colorbar(orientation='vertical', label=r'$\rho$')
+        plt.imshow(np.average(imgs,axis=0).transpose(), origin='lower',vmin=0.0,vmax=0.2,
+                   aspect='auto', extent=[xval[0], xval[1], yval[0], yval[1]])
+        plt.colorbar(orientation='vertical', label=r'$\rho$', ticks=cbartics)
+        plt.xlabel('$\ln(1 / \Delta_{ab})$')
+        plt.ylabel('$\ln(k_{t} / \mathrm{GeV})$')
         pdf.savefig()
         plt.close()
-        fig=plt.figure()
+        fig=plt.figure(figsize=(6,6))
         plt.title('reference')
-        plt.imshow(np.average(imgs_ref,axis=0).transpose(), origin='lower',vmin=0.0, vmax=0.7)
-        plt.colorbar(orientation='vertical', label=r'$\rho$')
+        plt.imshow(np.average(imgs_ref,axis=0).transpose(), origin='lower',vmin=0.0, vmax=0.2,
+                   aspect='auto', extent=[xval[0], xval[1], yval[0], yval[1]])
+        plt.colorbar(orientation='vertical', label=r'$\rho$', ticks=cbartics)
+        plt.xlabel('$\ln(1 / \Delta_{ab})$')
+        plt.ylabel('$\ln(k_{t} / \mathrm{GeV})$')
         pdf.savefig()
         plt.close()
-        fig=plt.figure()
+        fig=plt.figure(figsize=(6,6))
         plt.title('generated/reference')
         plt.imshow(np.divide(np.average(imgs,axis=0).transpose(), np.average(imgs_ref,axis=0).transpose()),
-                   origin='lower', vmin=0.5, vmax=1.5, cmap=cm.seismic)
+                   origin='lower', vmin=0.5, vmax=1.5, cmap=cm.seismic,
+                   aspect='auto', extent=[xval[0], xval[1], yval[0], yval[1]])
         plt.colorbar(orientation='vertical')
+        plt.xlabel('$\ln(1 / \Delta_{ab})$')
+        plt.ylabel('$\ln(k_{t} / \mathrm{GeV})$')
         pdf.savefig()
         plt.close()
         fig=plt.figure()
@@ -92,8 +106,8 @@ def plot_lund_with_ref(filename, reference, figname):
             gen_act.append(np.sum(imgs[i]))
         for i in range(len(imgs_ref)):
             ref_act.append(np.sum(imgs_ref[i]))
-        plt.hist(gen_act, bins=bins, color='C0', alpha=0.3, label='generated')
-        plt.hist(ref_act, bins=bins, color='C1', alpha=0.3, label='reference')
+        plt.hist(gen_act, bins=bins, density=True, color='C0', alpha=0.3, label='generated')
+        plt.hist(ref_act, bins=bins, density=True, color='C1', alpha=0.3, label='reference')
         plt.title('activated pixels')
         plt.xlim((0,50))
         plt.legend()
